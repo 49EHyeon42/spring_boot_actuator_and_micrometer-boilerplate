e07ef7ebccdd66618cf2f55ac527cab0e7fdfb97
49EHyeon42 <49ehyeon42@gmail.com>
2024-10-20 17:54:35 +0900

    init: 프로젝트 시작

---
+++ src/main/java/deb/ehyeon/actuator_and_micrometer/ActuatorAndMicrometerApplication.java
@@ -0,0 +1,12 @@
+package deb.ehyeon.actuator_and_micrometer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatorAndMicrometerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActuatorAndMicrometerApplication.class, args);
+	}
+}
